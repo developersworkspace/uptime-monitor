2ff421155e271ab5eb6d4a6746dcc2cff2c6245e
Barend Erasmus <developersworkspace@gmail.com>
2018-06-05 10:52:09 +0200

    update

--- service/src/repositories/website.ts
+++ service/src/repositories/website.ts
@@ -1,24 +1,48 @@
 import { injectable } from 'inversify';
+import * as mongodb from 'mongodb';
 import { Website } from '../entities/website';
 import { IWebsiteRepository } from '../interfaces/website-repository';
 
 @injectable()
 export class WebsiteRepository implements IWebsiteRepository {
 
-    private static websites: Website[] = [
-        new Website(new Date(), '1', 'Euromonitor', 'https://euromonitor.com'),
-    ];
-
     public async find(url: string, userId: string): Promise<Website> {
-        return WebsiteRepository.websites.find((website: Website) => website.url === url);
+        const client: mongodb.MongoClient = await mongodb.MongoClient.connect('mongodb://127.0.0.1:27017');
+
+        const database: mongodb.Db = client.db('uptime-monitor');
+
+        const collection: mongodb.Collection = database.collection('websites');
+
+        const result: any = await collection.findOne({
+            url,
+            userId,
+        });
+
+        return new Website(result.createdTimestamp, result.id, result.name, result.url);
     }
 
     public async findAll(userId: string): Promise<Website[]> {
-        return WebsiteRepository.websites;
+        const client: mongodb.MongoClient = await mongodb.MongoClient.connect('mongodb://127.0.0.1:27017');
+
+        const database: mongodb.Db = client.db('uptime-monitor');
+
+        const collection: mongodb.Collection = database.collection('websites');
+
+        const result: any[] = await collection.find({
+            userId,
+        }).toArray();
+
+        return result.map((x: any) => new Website(x.createdTimestamp, x.id, x.name, x.url));
     }
 
     public async insert(userId: string, website: Website): Promise<void> {
-        WebsiteRepository.websites.push(website);
+        const client: mongodb.MongoClient = await mongodb.MongoClient.connect('mongodb://127.0.0.1:27017');
+
+        const database: mongodb.Db = client.db('uptime-monitor');
+
+        const collection: mongodb.Collection = database.collection('websites');
+
+        await collection.insertOne(website);
     }
 
 }
